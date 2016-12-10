69953ddf99695a2507d04fc1858a2cfd007b8cbb
Ricardo <ricardovdheijden@hotmail.com>
2016-12-10 19:46:32 +0100

    iMonMenu: added scroll function and refresh function

--- Kodi-on-iMon/iMonMenu.Designer.cs
+++ Kodi-on-iMon/iMonMenu.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.lblLine2 = new System.Windows.Forms.Label();
+            this.lblLine1 = new System.Windows.Forms.Label();
+            this.tmrFormRefreshRate = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // lblLine2
+            // 
+            this.lblLine2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.lblLine2.Font = new System.Drawing.Font("Courier New", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLine2.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lblLine2.Location = new System.Drawing.Point(7, 131);
+            this.lblLine2.Name = "lblLine2";
+            this.lblLine2.Size = new System.Drawing.Size(270, 30);
+            this.lblLine2.TabIndex = 8;
+            // 
+            // lblLine1
+            // 
+            this.lblLine1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.lblLine1.Font = new System.Drawing.Font("Courier New", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLine1.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lblLine1.Location = new System.Drawing.Point(7, 101);
+            this.lblLine1.Name = "lblLine1";
+            this.lblLine1.Size = new System.Drawing.Size(270, 30);
+            this.lblLine1.TabIndex = 7;
+            // 
+            // tmrFormRefreshRate
+            // 
+            this.tmrFormRefreshRate.Interval = 250;
+            this.tmrFormRefreshRate.Tick += new System.EventHandler(this.tmrFormRefreshRate_Tick);
             // 
             // iMonMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblLine2);
+            this.Controls.Add(this.lblLine1);
             this.KeyPreview = true;
             this.Name = "iMonMenu";
             this.Text = "iMonMenu";
@@ -43,6 +74,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblLine2;
+        private System.Windows.Forms.Label lblLine1;
+        private System.Windows.Forms.Timer tmrFormRefreshRate;
 
     }
 }
---
+++ Kodi-on-iMon/iMonMenu.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kodi_on_iMon
+{
+    public partial class iMonMenu : Form
+    {
+        iMon imon;
+        string[] menu = {"item 1","item 2","item 3","item 4","item 5", "item 6", "item 7", "item 8"};
+        int cursorAt = 0;
+        
+        public iMonMenu()
+        {
+            InitializeComponent();
+
+            imon = new iMon();
+            imon.initialise();
+            imon.setRefreshRate(100);
+            imon.setScrollDelay(5000);
+            refreshScreen();
+
+            tmrFormRefreshRate.Interval = 100;
+            tmrFormRefreshRate.Enabled = true;
+        }
+
+        // direction 0 = up (cursorAt - 1)
+        // direction 1 = down (cursorAt + 1)
+        public void scroll(int direction)
+        {
+            if (direction == 0)
+            {
+                cursorAt--;
+                refreshScreen();
+            }
+            else if (direction == 1)
+            {
+                cursorAt++;
+                refreshScreen();
+            }
+        }
+
+        public void refreshScreen()
+        {
+            imon.setText("> " + menu[cursorAt],"  " + menu[cursorAt+1]);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Up)
+            {
+                scroll(0);
+            }
+            else if (keyData == Keys.Down)
+            {
+                scroll(1);
+            }
+            else if (keyData == Keys.Enter)
+            {
+                MessageBox.Show("You pressed Enter key");
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void tmrFormRefreshRate_Tick(object sender, EventArgs e)
+        {
+            lblLine1.Text = imon.getVFDText(0);
+            lblLine2.Text = imon.getVFDText(1);
+        }
+    }
+}
