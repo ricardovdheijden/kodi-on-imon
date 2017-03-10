bbb6dbbf1480b887043a74006f785d5a8e4b2305
Ricardo van der Heijden <ricardovdheijden@hotmail.com>
2017-03-10 11:51:06 +0100

    KodiMediaInfo: added empty window

---
+++ Kodi-on-iMon/KodiMediaInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kodi_on_iMon
+{
+    public partial class KodiMediaInfo : Form
+    {
+        public KodiMediaInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
